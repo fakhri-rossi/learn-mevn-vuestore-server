e8291f9e4cb0d57a745761ac7f5f788c93c93acd
Fakhri Rossi <rossifakhri@gmail.com>
2024-06-23 15:40:59 +0700

    Created config db, app models: index and product model

--- app.js
+++ app.js
@@ -5,6 +5,18 @@ const PORT = process.env.PORT || 8000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const db = require('./app/models');
+db.mongoose.connect(db.url)
+    .then((result) => {
+        console.log('Connected to mongodb');
+
+    }).catch((err) => {
+        console.log(err);
+        process.exit()
+    });
+
+
+
 app.get('/', (req, res) => {
     res.json({
         message: 'welcome to vuestore-server'
